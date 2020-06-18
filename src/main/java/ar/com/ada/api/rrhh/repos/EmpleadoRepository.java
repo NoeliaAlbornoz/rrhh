cdd47ca3a3b64e431bfb0413b479fbd6dec8b09b
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-06-18 03:21:32 -0300

    Se agregó método y query para filtrar empleados por el id de la categoria.

--- src/main/java/ar/com/ada/api/rrhh/repos/EmpleadoRepository.java
+++ src/main/java/ar/com/ada/api/rrhh/repos/EmpleadoRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import ar.com.ada.api.rrhh.entities.Empleado;
@@ -15,5 +16,8 @@ public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
 
     @Query("select e from Empleado e where e.nombre = ?1")
     List<Empleado> findByNombreEmpleado(String nombre); 
+
+    @Query("Select e FROM Empleado e WHERE e.categoria.categoriaId = :categoriaId")
+    List<Empleado> findAllByCategoriaId(@Param("categoriaId") int categoriaId);
 
 }
