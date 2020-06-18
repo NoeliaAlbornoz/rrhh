b944bc5e242c0c4f32692f5b2c92614cbb5cde68
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-06-18 00:29:21 -0300

    Se creó y completó la clase Categoria.java con atributos, anotaciones, getters y setters.

---
+++ src/main/java/ar/com/ada/api/rrhh/entities/Categoria.java
@@ -0,0 +1,38 @@
+package ar.com.ada.api.rrhh.entities;
+
+import java.math.BigDecimal;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @Column(name = "categoria_id")
+    private int categoriaId;
+    private String nombre;
+    @Column(name = "sueldo_base")
+    private BigDecimal sueldoBase;
+
+	public int getCategoriaId() {
+		return categoriaId;
+	}
+	public void setCategoriaId(int categoriaId) {
+		this.categoriaId = categoriaId;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public BigDecimal getSueldoBase() {
+		return sueldoBase;
+	}
+	public void setSueldoBase(BigDecimal sueldoBase) {
+		this.sueldoBase = sueldoBase;
+	}
+
+}
+
