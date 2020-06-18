23e406e5f049cfefd09decfb6151f89cc8219619
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-06-18 02:04:06 -0300

    Se creó y completó la clase EmpleadoControler con los métodos necesarios para realizar GET`S y POST.

---
+++ src/main/java/ar/com/ada/api/rrhh/controllers/EmpleadoController.java
@@ -0,0 +1,64 @@
+package ar.com.ada.api.rrhh.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ar.com.ada.api.rrhh.entities.*;
+import ar.com.ada.api.rrhh.models.response.GenericResponse;
+import ar.com.ada.api.rrhh.services.*;
+
+@RestController
+public class EmpleadoController {
+
+    @Autowired
+    EmpleadoService empleadoService;
+
+    @GetMapping("/empleados")
+    public List<Empleado> getEmpleados(@RequestParam(value = "nombre", required = false) String nombre) {
+        List<Empleado> le;
+
+        if (nombre == null) {
+            le = empleadoService.getEmpleados();
+        } else {
+            le = empleadoService.buscarEmpleadosPorNombre(nombre);
+        }
+
+        return le;
+    } 
+
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> getHuespedById(@PathVariable int id) {
+        Empleado e = empleadoService.buscarPorId(id);
+
+        if (e == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(e);
+    }
+
+    @PostMapping("/empleados")
+    public ResponseEntity<?> postEmpleado(@RequestBody Empleado req) {
+
+        GenericResponse r = new GenericResponse();
+
+        boolean resultado = empleadoService.crearEmpleado(req);
+
+        if (resultado) {
+            r.isOk = true;
+            r.id = req.getEmpleadoId();
+            r.message = "Creaste un empleado con éxito.";
+            return ResponseEntity.ok(r);
+        } else {
+
+            r.isOk = false;
+            r.message = "No se pudo crear un empleado! ya existe un empleado con ese ID.";
+
+            return ResponseEntity.badRequest().body(r);
+        }
+
+    }
+
+}
