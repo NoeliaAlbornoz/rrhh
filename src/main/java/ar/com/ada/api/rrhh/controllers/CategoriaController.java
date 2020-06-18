d5e5bb0628d3fd9324e9ea6a0000eaf0587ca557
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-06-18 00:33:52 -0300

    Se creó y completó la clase CategoriaController.java con atributo y métodos necesarios para realizar GET y POST.

---
+++ src/main/java/ar/com/ada/api/rrhh/controllers/CategoriaController.java
@@ -0,0 +1,53 @@
+package ar.com.ada.api.rrhh.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ar.com.ada.api.rrhh.entities.*;
+import ar.com.ada.api.rrhh.models.response.GenericResponse;
+import ar.com.ada.api.rrhh.services.CategoriaService;
+
+@RestController
+public class CategoriaController {
+
+    @Autowired
+    CategoriaService categoriaService;
+
+    @GetMapping("/categorias")
+    public List<Categoria> getCategorias(@RequestParam(value = "nombre", required = false) String nombre) {
+        List<Categoria> lc;
+
+        if (nombre == null) {
+            lc = categoriaService.getCategorias();
+        } else {
+            lc = categoriaService.buscarCategoriasPorNombre(nombre);
+        }
+
+        return lc;
+    }
+
+    @PostMapping("/categorias")
+    public ResponseEntity<?> postHuesped(@RequestBody Categoria req) {
+
+        GenericResponse r = new GenericResponse();
+
+        boolean resultado = categoriaService.crearCategoria(req);
+
+        if (resultado) {
+            r.isOk = true;
+            r.id = req.getCategoriaId();
+            r.message = "Creaste una categoría con éxito.";
+            return ResponseEntity.ok(r);
+        } else {
+
+            r.isOk = false;
+            r.message = "No se pudo crear la categoría! ya existe una categoría con ese ID.";
+
+            return ResponseEntity.badRequest().body(r);
+        }
+
+    }
+
+}
