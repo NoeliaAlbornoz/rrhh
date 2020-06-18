29fded382a13d681a716f26f3443b24d4c1d79d9
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-06-18 02:06:07 -0300

    Se creó y completó la clase EmpleadoService con el atributo y métodos necesarios para obtener, buscar y crear empleados.

---
+++ src/main/java/ar/com/ada/api/rrhh/services/EmpleadoService.java
@@ -0,0 +1,56 @@
+package ar.com.ada.api.rrhh.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.rrhh.entities.Empleado;
+
+import ar.com.ada.api.rrhh.repos.EmpleadoRepository;
+
+@Service
+public class EmpleadoService {
+
+    @Autowired
+    EmpleadoRepository empleadoRepo;
+
+    public List<Empleado> getEmpleados() {
+
+        return empleadoRepo.findAll();
+    }
+
+    public List<Empleado> buscarEmpleadosPorNombre(String nombre) {
+
+        return empleadoRepo.findByNombreEmpleado(nombre);
+    } 
+
+    public boolean crearEmpleado(Empleado empleado) {
+
+        if (existe(empleado.getEmpleadoId()))
+            return false;
+
+        grabar(empleado);
+        return true;
+    }
+
+    public void grabar(Empleado empleado) {
+
+        empleadoRepo.save(empleado);
+    }
+
+    public boolean existe(int empleadoId) {
+
+        Empleado empleado = buscarPorId(empleadoId);
+
+        return empleado != null;
+
+    }
+
+    public Empleado buscarPorId(int empleadoId) {
+
+        return empleadoRepo.findByEmpleadoId(empleadoId);
+    }
+    
+}
+
